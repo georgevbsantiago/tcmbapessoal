0863ce6736c0c5a69053e05e616e55ae7e45d15f
George Santiago <georgevbsantiago@hotmail.com>
2019-02-03 12:27:49 -0200

    Ajuste na identação do código

--- R/01_executar_web_scraping.R
+++ R/01_executar_web_scraping.R
@@ -107,110 +107,110 @@ executar_web_scraping <- function(ano_inicio, nome_scraping, sgbd = "sqlite",
   if (dir.exists(nome_scraping) == FALSE &
       file.exists(arq_rds_id_ws) == FALSE) {
     
-    # Função que cria o diretório com o nome do Web Scraping
-    dir.create(nome_scraping)
-    
-    # Função que define o diretório Raiz
-    setwd(nome_scraping)
-    
-    print("O Diretório principal foi criado com Sucesso!")
-    
-    # Função que define o Diretório Raiz e crias demais diretórios
-    tcmbapessoal::criar_diretorios()
-    
-    # Função que cria a identidade do Web Scraping
-    tcmbapessoal::criar_ws_id(nome_scraping, arq_rds_id_ws, sgbd)
-    
-    # Função que cria o Banco de Dados
-    tcmbapessoal::criar_bd(sgbd)
-    
-    # Função que cria 5 tabelas no Banco de Dados.
-    tcmbapessoal::criar_tabelas_bd(sgbd)
+          # Função que cria o diretório com o nome do Web Scraping
+          dir.create(nome_scraping)
+          
+          # Função que define o diretório Raiz
+          setwd(nome_scraping)
+          
+          print("O Diretório principal foi criado com Sucesso!")
+          
+          # Função que define o Diretório Raiz e crias demais diretórios
+          tcmbapessoal::criar_diretorios()
+          
+          # Função que cria a identidade do Web Scraping
+          tcmbapessoal::criar_ws_id(nome_scraping, arq_rds_id_ws, sgbd)
+          
+          # Função que cria o Banco de Dados
+          tcmbapessoal::criar_bd(sgbd)
+          
+          # Função que cria 5 tabelas no Banco de Dados.
+          tcmbapessoal::criar_tabelas_bd(sgbd)
     
   } else {
     
-    print("Executado Web Sacraping...")
+          print("Executado Web Sacraping...")
     
   }
   
   
   if(dir.exists(nome_scraping) == TRUE) {
     
-    setwd(nome_scraping)
-    
-    print("O Diretório Raiz foi definido com Sucesso!")
+          setwd(nome_scraping)
+          
+          print("O Diretório Raiz foi definido com Sucesso!")
     
   }
   
   
   if(file.exists(arq_rds_id_ws) == TRUE) {
   
-      info_ws <- readRDS(arq_rds_id_ws)
+          info_ws <- readRDS(arq_rds_id_ws)
+        
+          print(paste("Nome do Web Scraping:", info_ws$nome))
+          print(paste("Diretório do Web Scraping:", info_ws$dir_wd))
+          print(paste("SGBD do Web Scraping:", info_ws$sgbd_ws))
+          print(paste("Data de criação do Web Scraping:", info_ws$data_time_create))
+          
+          # Registrar a data e hora do início da execução do Web Scraping no arquivo RDS
+          inicio_exe_ws_data_time(arq_rds_id_ws)
+          
+          # ---------------------------------------------------------------------------
+      
     
-      print(paste("Nome do Web Scraping:", info_ws$nome))
-      print(paste("Diretório do Web Scraping:", info_ws$dir_wd))
-      print(paste("SGBD do Web Scraping:", info_ws$sgbd_ws))
-      print(paste("Data de criação do Web Scraping:", info_ws$data_time_create))
-      
-      # Registrar a data e hora do início da execução do Web Scraping no arquivo RDS
-      inicio_exe_ws_data_time(arq_rds_id_ws)
-      
-      # ---------------------------------------------------------------------------
-  
-
-      # Função que cria a tabela dCalendario
-      tcmbapessoal::criar_tb_dcalendario(ano_inicio, sgbd)
-      
-      # Função que faz o Web Scraping do código e nome dos Municípios
-      tcmbapessoal::criar_tb_dmunicipios(sgbd)
-      
-      # Função que obtém o código e nome das Entidades via Web Service
-      tcmbapessoal::criar_tb_dmunicipios_entidades(sgbd)
-      
-      # Função que cria a tabela das requisições
-      tcmbapessoal::criar_tb_requisicoes_pessoal(sgbd)
-      
-      # Função que faz o Web Scraping das páginas HTML que contêm os dados da Folha de Pessoal.
-      # Obs: O tempo de resposta do TCM está entre 10 a 30 segundos
-      tcmbapessoal::executar_scraping_html_folhapessoal(repetir, sgbd)
-      
-      # Função que faz o parser (extração) dos HTMLs e o Data Wrangling dos dados extraídos
-      # Faz o pré-processamento dos dados obtidos do HTML, aplicando o conceito Tidy Data
-      # Por fim, cria uma arquivos CSV para a pasta dados_exportados.
-      tcmbapessoal::executar_data_wrangling_html_pessoal(sgbd)
-
-              # O conceito Tidy Data de Hadley Wickham tem por objetivo arrumar os dados
-              # para que eles sejam utilizados em softwares de estatísticas ou
-              # de Business Intelligence sem a necessidade de realizar
-              # maiores transformações nos dados.
-
-              # O Conceito está resumido nestas três regras:
-              # - Cada variável deve ter sua própria coluna.
-              # - Cada observação deve ter sua própria linha.
-              # - Cada valor deve ter sua própria célula.
-
-              #(http://r4ds.had.co.nz/tidy-data.html)
-
-      # Função que faz o Backup local do Banco de Dados e dos arquivos HTML e CSV.
-      # e o Backup em nuvem no DropBox
-      tcmbapessoal::executar_backup_arquivos(backup_local, backup_nuvem)
-      
-      # Exportar os CSVs para o Dropbox para conectar ao Power BI
-      tcmbapessoal::exportar_csv_dropbox(exportar_nuvem)
-      
-      
-      # ---------------------------------------------------------------------------
-      
-      
-      # Registrar a data e hora do fim da execução do Web Scraping no arquivo RDS
-      fim_exe_ws_data_time(arq_rds_id_ws)
-
-      
-      print("## Web Scraping finalizado com sucesso! ###")
+          # Função que cria a tabela dCalendario
+          tcmbapessoal::criar_tb_dcalendario(ano_inicio, sgbd)
+          
+          # Função que faz o Web Scraping do código e nome dos Municípios
+          tcmbapessoal::criar_tb_dmunicipios(sgbd)
+          
+          # Função que obtém o código e nome das Entidades via Web Service
+          tcmbapessoal::criar_tb_dmunicipios_entidades(sgbd)
+          
+          # Função que cria a tabela das requisições
+          tcmbapessoal::criar_tb_requisicoes_pessoal(sgbd)
+          
+          # Função que faz o Web Scraping das páginas HTML que contêm os dados da Folha de Pessoal.
+          # Obs: O tempo de resposta do TCM está entre 10 a 30 segundos
+          tcmbapessoal::executar_scraping_html_folhapessoal(repetir, sgbd)
+          
+          # Função que faz o parser (extração) dos HTMLs e o Data Wrangling dos dados extraídos
+          # Faz o pré-processamento dos dados obtidos do HTML, aplicando o conceito Tidy Data
+          # Por fim, cria uma arquivos CSV para a pasta dados_exportados.
+          tcmbapessoal::executar_data_wrangling_html_pessoal(sgbd)
+    
+                  # O conceito Tidy Data de Hadley Wickham tem por objetivo arrumar os dados
+                  # para que eles sejam utilizados em softwares de estatísticas ou
+                  # de Business Intelligence sem a necessidade de realizar
+                  # maiores transformações nos dados.
+    
+                  # O Conceito está resumido nestas três regras:
+                  # - Cada variável deve ter sua própria coluna.
+                  # - Cada observação deve ter sua própria linha.
+                  # - Cada valor deve ter sua própria célula.
+    
+                  #(http://r4ds.had.co.nz/tidy-data.html)
+    
+          # Função que faz o Backup local do Banco de Dados e dos arquivos HTML e CSV.
+          # e o Backup em nuvem no DropBox
+          tcmbapessoal::executar_backup_arquivos(backup_local, backup_nuvem)
+          
+          # Exportar os CSVs para o Dropbox para conectar ao Power BI
+          tcmbapessoal::exportar_csv_dropbox(exportar_nuvem)
+          
+          
+          # ---------------------------------------------------------------------------
+          
+          
+          # Registrar a data e hora do fim da execução do Web Scraping no arquivo RDS
+          fim_exe_ws_data_time(arq_rds_id_ws)
+    
+          
+          print("## Web Scraping finalizado com sucesso! ###")
       
   } else {
     
-    stop("Ocorreu algum problema durante a execução do Web Scraping!!!")
+          stop("Ocorreu algum problema durante a execução do Web Scraping!!!")
     
   }
   
